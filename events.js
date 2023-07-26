e3401ec336ada4928cf454d61a50ff7b8b09dd15
Gokulsundar S <118619862+gokulsundars21cse@users.noreply.github.com>
2023-07-26 11:22:55 +0530

    Add files via upload

---
+++ events.js
@@ -0,0 +1,57 @@
+function opacity1(){
+    let f = document.getElementById("hackbuzz");
+    f.style.opacity = 0.1;
+    let g = document.getElementById("hbbtn1");
+    g.style.opacity = 1;
+}
+
+function noopacity1(){
+    let f = document.getElementById("hackbuzz");
+    f.style.opacity = 100;
+    let g = document.getElementById("hbbtn1");
+    g.style.opacity = 0;
+}
+
+function opacity2(){
+    let f = document.getElementById("codeconfitte");
+    f.style.opacity = 0.1;
+    let g = document.getElementById("hbbtn2");
+    g.style.opacity = 1;
+}
+
+function noopacity2(){
+    let f = document.getElementById("codeconfitte");
+    f.style.opacity = 100;
+    let g = document.getElementById("hbbtn2");
+    g.style.opacity = 0;
+}
+
+function opacity3(){
+    let f = document.getElementById("codecraft");
+    f.style.opacity = 0.1;
+    let g = document.getElementById("hbbtn3");
+    g.style.opacity = 1;
+}
+
+function noopacity3(){
+    let f = document.getElementById("codecraft");
+    f.style.opacity = 100;
+    let g = document.getElementById("hbbtn3");
+    g.style.opacity = 0;
+}
+
+function opacity4(){
+    let f = document.getElementById("developnet");
+    f.style.opacity = 0.1;
+    let g = document.getElementById("hbbtn4");
+    g.style.opacity = 1;
+}
+
+function noopacity4(){
+    let f = document.getElementById("developnet");
+    f.style.opacity = 100;
+    let g = document.getElementById("hbbtn4");
+    g.style.opacity = 0;
+}
+
+
